ad95e8c9d58497fc2479160b0859e9e6589531e9
Chad Wilken <chad.wilken@gmail.com>
2015-10-23 12:44:37 -0500

    Added in elasticsearch monitoring

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "monit_configs-cc"
 maintainer        "Chad Wilken"
 maintainer_email  "chad@companycam.com"
 description       "Monit configs for server components"
-version           "0.1.3"
+version           "0.1.4"
 
 recipe "monit_configs-cc::memcached", "Monit config for memcached"
 recipe "monit_configs-cc::nginx", "Monit config for nginx"

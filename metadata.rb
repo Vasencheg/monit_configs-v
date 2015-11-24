23734776bdb0e179b1fca209ac96e0b170ff59bd
Chad Wilken <chad.wilken@gmail.com>
2015-11-24 16:05:02 -0600

    Added in postgres monitoring configuration

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,12 @@ name              "monit_configs-cc"
 maintainer        "Chad Wilken"
 maintainer_email  "chad@companycam.com"
 description       "Monit configs for server components"
-version           "0.1.9"
+version           "0.1.10"
 
 recipe "monit_configs-cc::elasticsearch", "Monit config for elasticsearch"
 recipe "monit_configs-cc::memcached", "Monit config for memcached"
 recipe "monit_configs-cc::nginx", "Monit config for nginx"
 recipe "monit_configs-cc::redis-server", "Monit config for redis server"
+recipe "monit_configs-cc::postgres-server", "Monit config for postgres server"
 
 supports "ubuntu"

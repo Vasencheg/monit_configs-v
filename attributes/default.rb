a0406f75133f607dbd7370aabc2d84dd428c40b9
Chad Wilken <chad.wilken@gmail.com>
2016-03-09 11:28:24 -0600

    Added in mongo config

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,2 @@
 default['monit_configs-cc']['postgres']['pidfile'] = '/var/run/postgresql/9.3-main.pid'
-# for the community cookbook use '/var/lib/mongo/mongod.lock'
-#default['monit_configs-cc']['mongo']['pidfile'] = '/var/run/mongo/mongo.pid'
+default['monit_configs-cc']['mongo']['pidfile'] = '/var/run/mongo/mongo.pid'
